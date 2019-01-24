8f6b52d937f1398ccb19b20e1fd3aad4d50fed2d
Chiranjeeb Mahanta <chiranjeebm2410@gmail.com>
2019-01-24 22:09:20 +0530

    added settingsForm to field widget

--- src/Plugin/Field/FieldWidget/FieldCollectionTable.php
+++ src/Plugin/Field/FieldWidget/FieldCollectionTable.php
@@ -6,6 +6,11 @@
 namespace Drupal\field_collection_table\Plugin\Field\FieldWidget;
 
 use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Annotation\Plugin;
+use Drupal\Core\Annotation\Translation;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * @FieldWidget(
@@ -25,5 +30,32 @@ class FieldCollectionTable extends WidgetBase {
    * https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Field%21WidgetInterface.php/interface/WidgetInterface/8
    * https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Field%21WidgetBase.php/class/WidgetBase/8
    */
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, array &$form_state) {
+
+    $element['nodragging'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Disable drag and drop'),
+      '#description' => $this->t('If checked, users cannot rearrange the rows.'),
+      '#default_value' => $settings['nodragging'],
+      '#weight' => 2,
+    );
+
+    $element['hide_title'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Hide title'),
+      '#description' => $this->t('If checked, the field title will be hidden.'),
+      '#default_value' => $this->getSetting['hide_title'],
+      '#weight' => 3,
+    );
+
+    return $element;
+
+  }
+
+
 
 }
