d922b801d0e19691e17b260c31a77b0c38d32596
Bhanu Prakash Poluparthi <32234113+BhanuPrakashNani@users.noreply.github.com>
2019-01-28 22:24:58 +0530

    Update FieldCollectionTable.php

--- src/Plugin/Field/FieldWidget/FieldCollectionTable.php
+++ src/Plugin/Field/FieldWidget/FieldCollectionTable.php
@@ -2,11 +2,13 @@
  * @file
  * Contains \Drupal\field_collection_table\Plugin\Field\FieldWidget\FieldCollectionTable.
  */
-
 namespace Drupal\field_collection_table\Plugin\Field\FieldWidget;
-
 use Drupal\Core\Field\WidgetBase;
-
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Annotation\Plugin;
+use Drupal\Core\Annotation\Translation;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Form\FormStateInterface;
 /**
  * @FieldWidget(
  *  id = "field_collection_table",
@@ -15,7 +17,6 @@ use Drupal\Core\Field\WidgetBase;
  * )
  */
 class FieldCollectionTable extends WidgetBase {
-
   /**
    * @FIXME
    * Move all logic relating to the field_collection_table widget into this class.
@@ -25,5 +26,24 @@ class FieldCollectionTable extends WidgetBase {
    * https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Field%21WidgetInterface.php/interface/WidgetInterface/8
    * https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Field%21WidgetBase.php/class/WidgetBase/8
    */
-
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, array &$form_state) {
+    $element['nodragging'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Disable drag and drop'),
+      '#description' => $this->t('If checked, users cannot rearrange the rows.'),
+      '#default_value' => $settings['nodragging'],
+      '#weight' => 2,
+    );
+    $element['hide_title'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Hide title'),
+      '#description' => $this->t('If checked, the field title will be hidden.'),
+      '#default_value' => $this->getSetting['hide_title'],
+      '#weight' => 3,
+    );
+    return $element;
+  }
 }
