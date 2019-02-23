ad99ca8c5370aad2d253afa8d8ce8b816b26be5d
bhanuprakashnani <p.bhanuprakash12345@gmail.com>
2019-02-23 18:15:38 +0530

    added formatters

--- src/Plugin/Field/FieldFormatter/FieldCollectionTableView.php
+++ src/Plugin/Field/FieldFormatter/FieldCollectionTableView.php
@@ -1,4 +1,6 @@
-<?php /**
+<?php
+
+/**
  * @file
  * Contains \Drupal\field_collection_table\Plugin\Field\FieldFormatter\FieldCollectionTableView.
  */
@@ -22,14 +24,119 @@ use Drupal\Core\Form\FormStateInterface;
 class FieldCollectionTableView extends FormatterBase {
 
   /**
-   * @FIXME
-   * Move all logic relating to the field_collection_table_view formatter into this
-   * class. For more information, see:
-   *
-   * https://www.drupal.org/node/1805846
-   * https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Field%21FormatterInterface.php/interface/FormatterInterface/8
-   * https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Field%21FormatterBase.php/class/FormatterBase/8
+   * {@inheritdoc}
    */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+
+    $element = [];
+    $field_options = ['none' => $this->t('None')];
+
+    $element['hide_empty'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Hide empty collection'),
+      '#default_value' => $this->getSetting('hide_empty'),
+      '#description' => $this->t('If enabled, nothing will be displayed for an empty collection (not even the add link).'),
+    ];
+
+    $element['empty'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Hide empty columns'),
+      '#description' => $this->t('If checked, hides empty table columns.'),
+      '#default_value' => $this->getSetting('empty'),
+    ];
+
+    $element['caption'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Table caption'),
+      '#description' => $this->t('Displayed in the caption element above the table'),
+      '#default_value' => $this->getSetting('caption'),
+    ];
+
+    $element['orientation'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Orientation'),
+      '#description' => $this->t('Set the orientation of the table'),
+      '#options' => [
+        'columns' => $this->t('Columns'),
+        'rows' => $this->t('Rows'),
+      ],
+      '#default_value' => $this->getSetting('orientation'),
+    );
+
+    $element['header_column'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Header field'),
+      '#description' => $this->t('The selected field value will be used as the horizontal table header'),
+      '#options' => $field_options,
+      '#states'=> ['visible' => [':input[name="fields[field_fc]
+      [settings_edit_form][settings][orientation]"]' => ['value' => 'rows']]],
+    ];
+
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $element = [];
+    $settings = $this->getFieldSettings();
+
+    foreach ($items as $delta => $item) {
+      // Render each element as markup.
+      $element[$delta] = ['#markup' => $item->value];
+    }
+
+    if (empty($items) && !empty($this->getSetting('hide_empty'))) {
+      return $element;
+    }
+
+/**
+// todo: modify the table method callback functions to sync with d8
+    if ($settings['orientation'] === 'columns') {
+      _field_collection_table_column_mode($element, $settings, $entity_type,
+        $entity, $field, $instance, $langcode, $items, $display);
+    }
+    if ($settings['orientation'] === 'rows') {
+      _field_collection_table_row_mode($element, $settings, $entity_type,
+        $entity, $field, $instance, $langcode, $items, $display);
+    }
+*/
+
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary()  {
+
+    $output = settingsSummary();
+
+    $output .= '<br>';
+    $output .= !empty($this->getSetting('hide_empty')) ? $this->t('Empty collections: Hidden') : $this->t('Empty collections: Shown');
+    $output .= '<br>';
+    $output .= !empty($this->getSetting('empty')) ? $this->t('Empty columns: Hidden') : $this->t('Empty columns: Shown');
+    $output .= !empty($this->getSetting('caption')) ? '<br>' . $this->t('Caption: %caption', ['%caption' => $this->t($this->getSetting('caption'))]) : '';
+    $orientations = ['columns' => $this->t('Column'), 'rows' => $this->t('Row')];
+    $output .= '<br />';
+    $output .= !empty($this->getSetting('empty')) ? $this->t('Empty columns: Hidden') : $this->t('Empty columns: Shown');
+    if (isset($this->getSetting('orientation'))) {
+      $output .= '<br />';
+      $output .= $this->t('Format fields as <strong>!orientation</strong>.', ['!orientation' => $orientations[$this->getSetting('orientation')]]);
+    }
+    if (isset($this->getSetting('orientation')) && $this->getSetting('orientation') === 'rows') {
+      $output .= '<br />';
+      if (isset($this->getSetting('header_column')) && $this->getSetting('header_column') !== 'none') {
+        $output .= '<br />';
+        $output .= $this->t('Field @field value is used as the header', ['@field' => $this->getSetting('header_column')]);
+      }
+    }
+
+    return $output;
+  }
+
+
 
 
 }
